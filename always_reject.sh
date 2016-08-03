d7ab3d99783054bdd5037f01d4f047e0007d90fb
James Garcia <james.rob.garcia@hpe.com>
2016-08-03 11:23:02 -0500

    Added formatting to the response message

--- always_reject.sh
+++ always_reject.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-echo "ERROR: Rejecting all pushes. Please contact repository admin for additional information."
+echo " ";
+echo "ERROR:  +------------------------------------------------------------+";
+echo "ERROR:  |  The 'Reject All Pushes' pre-receive hook is currently     |";
+echo "ERROR:  |  enabled on this repository. This will reject all pushes.  |";
+echo "ERROR:  |  Please contact the repository administrator if you need   |";
+echo "ERROR:  |  to disable this hook.                                     |";
+echo "ERROR:  +------------------------------------------------------------+";
+echo " ";
 
-exit 1
+exit 1;
+
