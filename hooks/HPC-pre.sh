c4bd58514310e3b79b246150e2a93bbd94396f7e
Michel Bourget <meshell@hpe.com>
2017-11-29 20:22:04 -0500

    Create HPC-pre.sh
    This is a copy of https://github.hpe.com/HPC/ghe/blob/master/hooks/ghe/HPC-pre.sh

---
+++ hooks/HPC-pre.sh
@@ -0,0 +1,23 @@
+ #! /bin/sh
+
+# NOTE: This hook requires the following alpine+openssh environment:
+#       docker/hpc.pre.receive.false.tar.gz
+
+SSH_OPTIONS="-q -x -o UserKnownHostsFile=/dev/null -o strictHostKeyChecking=no"
+SSH_USER=ghe
+SSH_HOST=salsa.engr.sgi.com
+SSH_HOST=137.38.153.239		# XXX: HPE yet do not DNS resolve sgi.com 
+SSH_ACCESS="${SSH_USER}@${SSH_HOST}"
+SSH="ssh $SSH_OPTIONS $SSH_ACCESS"
+
+ERR=0
+while read old_rev new_rev ref ; do
+	$SSH hooks pre  					\
+		${GITHUB_USER_LOGIN:-"user_unknown"}		\
+		${GITHUB_REPO_NAME:-"NA/repo_unknown"}		\
+		$old_rev					\
+		$new_rev					\
+		$ref
+	ERR=$(( $ERR + $? ))
+done
+exit $ERR
