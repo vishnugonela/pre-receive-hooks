6147ae7f3b3667d65a9c150d7ed46a5c7d12fea8
Jasmeen kamboj <jasmeen.kamboj@hpe.com>
2019-09-01 19:44:11 +0530

    Updating script to mandate commit message with jira id in pull request  heading

---
+++ hooks/verify-commit-msg.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Name: Verify 1st commit message(Heading of pull request) in git push
+# Description: Reject pushes that contain commit with message that does not adhere to defined regex. Useful to link github commits to jira issues
+#
+# Author: Jasmeen Kamboj <jasmeen.kamboj@hpe.com>
+# Organization Unit: Infosight
+# Team: Devops
+#
+
+set -e
+
+zero_commit='0000000000000000000000000000000000000000'
+
+while  read -r old_rev new_rev ref ; do
+
+  #Ingnore the  push if brach/tag got deleted
+  [ $new_rev = $zero_commit ] && continue
+
+  #Calculate commit range for updated branch
+  [ $old_rev = $zero_commit ] && range=$new_rev || range=$old_rev..$new_rev
+
+  for commit in $(git rev-list $range --reverse) ; do
+    if ! git log --max-count=1 --format=%B $commit | grep -e "[A-Z]\+-\\d\+"; then
+      echo "ERROR"
+      echo "Your push was rejected because"
+      echo "ERROR: $commit in ${ref#refs/heads/}"
+      echo "ERROR: is missing the JIRA id"
+      echo "ERROR:"
+      echo "ERROR: Please fix the commit message and push again."
+      echo "ERROR: https://help.github.com/en/articles/changing-a-commit-message"
+      echo "ERROR"
+      exit 1
+    fi
+    break
+  done
+done
