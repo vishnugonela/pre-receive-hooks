04b8193896a4e2f06affc62bf3b64b4245e52feb
Jasmeen kamboj <jasmeen.kamboj@hpe.com>
2019-10-10 15:37:27 +0530

    Updating syntax to verify commit message format

--- hooks/verify-commit-msg.sh
+++ hooks/verify-commit-msg.sh
@@ -22,7 +22,7 @@ while  read -r old_rev new_rev ref ; do
   [ $old_rev = $zero_commit ] && range=$new_rev || range=$old_rev..$new_rev
 
   for commit in $(git rev-list $range --reverse) ; do
-    if ! git log --max-count=1 --format=%B $commit | grep -e "\(IS\|ISHD\|ESC\|ICSE\)-\d\+"; then
+    if ! `git log --max-count=1 --format=%B $commit` | grep -e "\(IS\|ISHD\|ESC\|ICSE\)-\d\+"; then
       echo "ERROR"
       echo "Your push was rejected because"
       echo "ERROR: $commit in ${ref#refs/heads/}"
