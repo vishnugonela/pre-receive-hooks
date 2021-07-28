4ee7b6b1b88ad8f3941abd6b3b146b6fac47da23
Abhishikha Gupta <abhishikha.gupta@hpe.com>
2021-07-28 11:47:44 -0400

    Create ez-commit-msg.sh

---
+++ hooks/ez-commit-msg.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Name: EZ Pre-commit check
+# Description: Reject any push to main/integration/develop/release* branches which do not have JIRA reference in the comment.
+#
+# Organization Unit: Ezmeral
+# Team: Ezmeral
+#
+
+pattern='master|develop|main|integration'
+
+
+zero_commit='0000000000000000000000000000000000000000'
+msg_regex='/*(EZASK|EZAWB|EZCP|EZCPQA|EZCTL|EZDO|EZDT|EZEPIC|EZESC|EZID|EZIMG|EZIT|EZKD|EZKDF|EZKP|EZKUBE|EZML|EZPDM|EZQE|EZSPA|EZUX)-.+?'
+if [[ $local_branch =~ $pattern  ]];then
+  while read -r oldrev newrev refname; do
+
+    # Branch or tag got deleted, ignore the push
+      [ "$newrev" = "$zero_commit" ] && continue
+
+      # Calculate range for new branch/updated branch
+      [ "$oldrev" = "$zero_commit" ] && range="$newrev" || range="$oldrev..$newrev"
+
+    for commit in $(git rev-list "$range" --not --all); do
+      if ! git log --max-count=1 --format=%B $commit | grep -iqE "$msg_regex"; then
+        echo "ERROR:"
+        echo "ERROR: Your push was rejected because the commit"
+        echo "ERROR: $commit in ${refname#refs/heads/}"
+        echo "ERROR: is missing the JIRA Issue Please enter valid JIRA Issue ID for example EZCP-XXXX (Valid Regex allowed: $msg_regex)"
+        echo "ERROR:"
+        echo "ERROR: Please fix the commit message and push again."		
+        echo "ERROR"
+        exit 1
+      fi
+    done
+
+  done
+fi
