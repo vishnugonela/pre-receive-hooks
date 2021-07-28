c5409b0f5168ed0f01a61030893142ae0db4b979
Abhishikha Gupta <abhishikha.gupta@hpe.com>
2021-07-28 16:45:30 -0400

    Update ez-commit-msg.sh

--- hooks/ez-commit-msg.sh
+++ hooks/ez-commit-msg.sh
@@ -14,8 +14,7 @@ zero_commit='0000000000000000000000000000000000000000'
 msg_regex='/*(EZAWB|EZCP|EZCPQA|EZCTL|EZEPIC|EZESC)-.+?'
 
 while read -r oldrev newrev refname; do
-  current_branch=$refname
-  short_current_branch="$(echo $current_branch | sed 's/refs\/heads\///g')"
+  current_branch=${refname#refs/heads/}
   if [[ $current_branch =~ $pattern  ]];then
     # Branch or tag got deleted, ignore the push
       [ "$newrev" = "$zero_commit" ] && continue
@@ -23,14 +22,16 @@ while read -r oldrev newrev refname; do
       # Calculate range for new branch/updated branch
       [ "$oldrev" = "$zero_commit" ] && range="$newrev" || range="$oldrev..$newrev"
 
-    for commit in $(git rev-list "$range" --not --all); do
-      if ! git log --max-count=1 --format=%B $commit | grep -iqE "$msg_regex"; then
+    for commit in $(git rev-list "$range" --reverse); do
+      message=$(git show -s --format=%B $commit)
+      if ! echo $message | grep -iqE "$msg_regex"; then
         echo "ERROR:"
         echo "ERROR: Your push was rejected because the commit"
         echo "ERROR: $commit in ${refname#refs/heads/}"
         echo "ERROR: is missing the JIRA Issue Please enter valid JIRA Issue ID for example EZCP-XXXX (Valid Regex allowed: $msg_regex)"
         echo "ERROR:"
         echo "ERROR: Please fix the commit message and push again."		
+        echo "ERROR: https://help.github.com/en/articles/changing-a-commit-message"
         echo "ERROR"
         exit 1
       fi
